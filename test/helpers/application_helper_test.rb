b009f839dbab89ed0ae71b8eae6a9df8281eedaa
蓮田哲也 <teppiro@hasudatetsuyanoMacBook-Air.local>
2020-04-28 16:02:31 +0900

    fix helper

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), page_title + " | " + base_title
-  end
-end
